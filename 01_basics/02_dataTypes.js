42f45035a6194056705a7c716d73e0a65ded0605
Fejal Sumra <162971378+fejal35@users.noreply.github.com>
2025-04-30 12:02:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using NodeJS, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("ABC")
+
+
+let name ="ABC"
+let age = 20
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/fales
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "abc"); // string
+console.log(typeof 12); // number
+console.log(typeof true); // boolean
+console.log(typeof 9909950867); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof Object); // function
